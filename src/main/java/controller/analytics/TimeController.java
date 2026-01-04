dccaa0e58b7f9dd75a6df98dfb30e3bdb4f43029
Jannick Wehmeier <jwehmeier@techfak.uni-bielefeld.de>
2026-01-04 16:58:45 +0100

    Added Heatmap that shows the messages per Weekday and Hour

--- src/main/java/controller/analytics/TimeController.java
+++ src/main/java/controller/analytics/TimeController.java
@@ -2,12 +2,12 @@ package controller.analytics;
 
 import controller.AnalyticsController;
 import model.MessageListFilter;
+import org.jfree.data.xy.DefaultXYZDataset;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
+import java.time.DayOfWeek;
+import java.util.*;
 
 public class TimeController extends AnalyticsController {
 
@@ -30,5 +30,37 @@ public class TimeController extends AnalyticsController {
             timeMessages.put(String.valueOf(i) + ":00-" + String.valueOf(i) + ":59", messageCount);
         }
         return timeMessages;
+    }
+
+    public DefaultXYZDataset getTimeWeekdayMessages(Set<String> selectedChatters) {
+
+        DefaultXYZDataset dataset = new DefaultXYZDataset();
+
+        List<Double> xList = new ArrayList<>();
+        List<Double> yList = new ArrayList<>();
+        List<Double> zList = new ArrayList<>();
+
+        for (int i =0; i < 24; i++) {
+            for (DayOfWeek day : DayOfWeek.values()) {
+                Integer messageCount = 0;
+                Map<String, Integer> messageResults = currentChatInfo.callMessages(
+                        new MessageListFilter(null, String.valueOf(i), day));
+                for (Map.Entry<String, Integer> entry : messageResults.entrySet()) {
+                    if (selectedChatters.contains(entry.getKey())) {
+                        messageCount += entry.getValue();
+                    }
+                }
+                xList.add((double) i);
+                yList.add((double) day.getValue() - 1);
+                zList.add((double) messageCount);
+            }
+        }
+        dataset.addSeries("Heatmap", new double[][] {
+                xList.stream().mapToDouble(Double::doubleValue).toArray(),
+                yList.stream().mapToDouble(Double::doubleValue).toArray(),
+                zList.stream().mapToDouble(Double::doubleValue).toArray()
+        });
+
+        return dataset;
     }
 }
--- src/main/java/view/analyticsPanels/TimePanel.java
+++ src/main/java/view/analyticsPanels/TimePanel.java
@@ -1,11 +1,27 @@
 package view.analyticsPanels;
 
 import controller.analytics.TimeController;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.GrayPaintScale;
+import org.jfree.chart.renderer.PaintScale;
+import org.jfree.chart.renderer.xy.XYBlockRenderer;
+import org.jfree.chart.title.PaintScaleLegend;
+import org.jfree.chart.ui.RectangleEdge;
+import org.jfree.data.xy.DefaultXYZDataset;
 import view.AnalyticsPanel;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.FieldPosition;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+import java.time.DayOfWeek;
 import java.util.Set;
 
 public class TimePanel extends AnalyticsPanel {
@@ -24,6 +40,7 @@ public class TimePanel extends AnalyticsPanel {
 
         JPanel chekBoxesPanel =  new JPanel();
         JPanel chartPanel =  new JPanel();
+        JPanel chart2Panel =  new JPanel();
 
         for (String chatter : chatters) {
             JCheckBox checkBox = new JCheckBox(chatter, true);
@@ -40,6 +57,10 @@ public class TimePanel extends AnalyticsPanel {
 
                 addBarGraphFromMap(timeController.getTimeMessages(selectedChatters),
                         "Messages by Hour", name, "Hours", 2, chartPanel);
+
+                chart2Panel.removeAll();
+
+                addHeatmap(chart2Panel);
             });
 
             chekBoxesPanel.add(checkBox);
@@ -50,5 +71,81 @@ public class TimePanel extends AnalyticsPanel {
 
         add(chekBoxesPanel);
         add(chartPanel);
+
+        addHeatmap(chart2Panel);
+        add(chart2Panel);
+    }
+
+    private void addHeatmap(JPanel panel) {
+        DefaultXYZDataset dataset = timeController.getTimeWeekdayMessages(selectedChatters);
+
+        XYBlockRenderer renderer = new XYBlockRenderer();
+        renderer.setBlockWidth(1.0);
+        renderer.setBlockHeight(1.0);
+
+        double max = 1;
+
+        for (int series = 0; series < dataset.getSeriesCount(); series++) {
+            for (int item = 0; item < dataset.getItemCount(series); item++) {
+                Number z = dataset.getZ(series, item);
+                if (z != null) {
+                    max = Math.max(max, z.doubleValue());
+                }
+            }
+        }
+
+        PaintScale paintScale = new GrayPaintScale(0, max);
+        renderer.setPaintScale(paintScale);
+
+        NumberAxis xAxis = new NumberAxis("Hour");
+        xAxis.setRange(-0.5, 23.5);
+        xAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+        NumberAxis yAxis = new NumberAxis("Weekday");
+        yAxis.setRange(-0.5, 6.5);
+        yAxis.setTickUnit(new NumberTickUnit(1));
+        yAxis.setInverted(true);
+
+        yAxis.setNumberFormatOverride(new NumberFormat() {
+            @Override
+            public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
+                // Monday = 0 → DayOfWeek.of(1)
+                return toAppendTo.append(DayOfWeek.of((int) number + 1).toString());
+            }
+            @Override public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
+                return format((double) number, toAppendTo, pos);
+            }
+            @Override public Number parse(String source, ParsePosition parsePosition) {
+                return null;
+            }
+        });
+
+        XYPlot plot = new XYPlot(dataset, xAxis, yAxis, renderer);
+        plot.setBackgroundPaint(Color.WHITE);
+
+        JFreeChart chart = new JFreeChart(
+                "Messages by Hour & Weekday",
+                JFreeChart.DEFAULT_TITLE_FONT,
+                plot,
+                false
+        );
+        chart.setBackgroundPaint(Color.WHITE);
+
+        PaintScaleLegend legend = new PaintScaleLegend(
+                paintScale,
+                new NumberAxis("Messages")
+        );
+
+        legend.setPosition(RectangleEdge.BOTTOM);
+        legend.setStripWidth(8);
+        legend.setPadding(0,10,0,10);
+        chart.addSubtitle(legend);
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(1000, 450));
+
+        panel.add(chartPanel);
+
+        panel.updateUI();
     }
 }
